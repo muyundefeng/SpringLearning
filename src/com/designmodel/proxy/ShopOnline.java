95c91c5632400a7452d25c723fcacca46a6fe5f7
lisheng <2533604335@qq.com>
2017-03-16 10:28:22 +0800

    增加代理模式说明

--- src/com/designmodel/proxy/ShopOnline.java
+++ src/com/designmodel/proxy/ShopOnline.java
@@ -1,6 +1,12 @@
 package com.designmodel.proxy;
 
-/**代理模式，模拟代购网站的情景
+/**非代理模式：
+ * client--->realObject
+ * 代理模式：
+ * client--->proxyObject---->realObject
+ * 在代理对象中提供一个与真实对象相同的接口，以便在任何时候都可以替代真实对象；
+ *
+ * 代理模式，模拟代购网站的情景
  * 定义抽象接口，真实对象与代理对象都要实现该类
  * Created by lisheng on 17-3-16.
  */
