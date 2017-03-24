811a44a984c6c5d48bb80198f206928f9c274bc3
lisheng <2533604335@qq.com>
2017-03-24 11:07:22 +0800

    lettecode

---
+++ src/Code/GenerateTrees.java
@@ -0,0 +1,36 @@
+//package Code;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by lisheng on 17-3-7.
+// */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//
+//    TreeNode(int x) {
+//        val = x;
+//    }
+//}
+//
+//public class GenerateTrees {
+//
+//    public static List<TreeNode> generateTrees(int n) {
+//        List<Integer> integers = new ArrayList<>();
+//        for (int i = 0; i < n; i++)
+//            integers.add(i);
+//    }
+//
+//    public static TreeNode buildTree(List<Integer> nums) {
+//        if (nums.size() == 1) {
+//            return new TreeNode(nums.get(0));
+//        }else{
+//            TreeNode node = new TreeNode(nums.get(0));
+//
+//        }
+//    }
+//
+//}
